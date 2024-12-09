b6d1450b34e8187318d53d9b6883acd9c0cb5693
Alvarizqi <alvarizki80@students.unnes.ac.id>
2024-12-09 20:50:02 +0700

    update favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
           name="twitter:image"
           content="https://alvarizqi.com/assets/twitter-image.jpg"
         />
-        <link rel="icon" href="/favicon.png?v=2" type="image/png" />
+        <link rel="icon" href="/favicon.ico?v=2" />
 
         <title>{metadata.title}</title>
       </head>
