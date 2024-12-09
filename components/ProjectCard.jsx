4390b9bda0c27b68c98ac9afd18cb5cb401ba76c
Alvarizqi <alvarizki80@students.unnes.ac.id>
2024-12-09 20:56:52 +0700

    update framer motion

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { FaCode, FaEye } from "react-icons/fa";
 import { FaSpinner } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const ProjectCard = ({ image, title, desc, preview, icon, demo }) => {
   const [loaded, setLoaded] = useState(false);
@@ -18,11 +19,20 @@ const ProjectCard = ({ image, title, desc, preview, icon, demo }) => {
   return (
     <div className="shadow-xl border-accent border-4 rounded-2xl w-full sm:max-w-[25rem] h-auto sm:h-[400px] flex flex-col justify-between mx-auto relative">
       {!loaded && (
-        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 rounded-2xl">
+        <motion.div
+          className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 rounded-2xl"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          transition={{
+            delay: 1,
+            duration: 0.4,
+            ease: "easeInOut",
+          }}
+        >
           <FaSpinner className="text-black text-4xl animate-spin" />
-        </div>
+        </motion.div>
       )}
-      <div
+      <motion.div
         className={`w-full h-[280px] sm:h-[240px] rounded-t-xl relative group transition-opacity duration-500 ${
           loaded ? "opacity-100" : "opacity-0"
         }`}
@@ -30,6 +40,9 @@ const ProjectCard = ({ image, title, desc, preview, icon, demo }) => {
           background: `url(${image}) center center/cover no-repeat`,
           visibility: loaded ? "visible" : "hidden",
         }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.4, ease: "easeInOut" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-accent gap-8 bg-opacity-0 rounded-t-xl hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
@@ -45,14 +58,19 @@ const ProjectCard = ({ image, title, desc, preview, icon, demo }) => {
             <FaEye className="h-10 w-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black/80 cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
-      </div>
-      <div
+      </motion.div>
+
+      {/* Project description */}
+      <motion.div
         className={`rounded-b-xl bg-[#DFD3C3] min-h-[10rem] py-2 px-6 flex flex-col justify-between transition-opacity duration-500 ${
           loaded ? "opacity-100" : "opacity-0"
         }`}
         style={{
           visibility: loaded ? "visible" : "hidden",
         }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.4, ease: "easeInOut" }}
       >
         <div>
           <h5 className="text-xl text-center font-semibold text-black mb-1">
@@ -63,7 +81,7 @@ const ProjectCard = ({ image, title, desc, preview, icon, demo }) => {
           </p>
         </div>
         <div className="flex flex-row gap-4 mt-10">{icon}</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
